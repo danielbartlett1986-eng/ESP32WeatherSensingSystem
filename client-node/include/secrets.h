a03ff45025c15a896fe578930e649e5a211a7ad2
dandanattack <dman5000@ymail.com>
2026-01-11 14:34:55 -0800

    Fix client-node gitlink and add as a normal directory

---
+++ client-node/include/secrets.h
@@ -0,0 +1,11 @@
+// secrets.h (DO NOT COMMIT THIS FILE)
+
+#pragma once
+
+// ---------- WiFi ----------
+#define WIFI_SSID      "DasHausIot"
+#define WIFI_PASSWORD  "Tundrav8!"
+
+// ---------- Server ----------
+static const char* serverIP = "192.168.10.7";   // INSIDE ESP32
+static const int serverPort = 80;
